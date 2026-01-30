74ad7d65d2c3969739f3e76ef5d1a3fb9a42e452
Peter Schmidt <schmidt.multimedia@gmail.com>
2026-01-30 16:08:59 +0100

    update / clean up
    Removed commented-out code and unnecessary iterations.

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -8,20 +8,6 @@ public class App {
         Worker worker = new Worker();
         Drone drone = new Drone();   
 
-        /* 1. Iteration  */
-
-        // output(queen.doYourJob());
-        // output(queen.fly());
-        // output("-------------------");
-
-        // output(worker.doYourJob());
-        // output(worker.fly());
-        // output("-------------------");
-
-        // output(drone.doYourJob());
-        // output(drone.fly());
-        // output("-------------------");	
-
         pollObj(queen);
         pollObj(worker);
         pollObj(drone);
@@ -29,32 +15,10 @@ public class App {
          //-- Das sollte eigentlich nicht gehen!--//
         HoneyBee bee = new HoneyBee(); // SuperKlasse!
         pollObj(bee);
-
-
-        
-        
+     
     }
 
-     /* 2. Iteration überladene Methoden*/  
-
-    // private static void pollObj(Queen obj){
-    //     output(obj.doYourJob());
-    //     output(obj.fly());
-    //     output("-------------------");
-    // }
-
-    //     private static void pollObj(Worker obj){
-    //     output(obj.doYourJob());
-    //     output(obj.fly());
-    //     output("-------------------");
-    // }
-
-    //     private static void pollObj(Drone obj){
-    //     output(obj.doYourJob());
-    //     output(obj.fly());
-    //     output("-------------------");
-    // }
-
+   
     /* 3. Iteration Polymorphie */  
 
     private static void pollObj(HoneyBee obj){
@@ -62,11 +26,6 @@ public class App {
         output(obj.fly());
         output("-------------------");
     }
-
-
-
-
-
 
    
     private static void output(String outputStr) {
@@ -76,4 +35,5 @@ public class App {
 
 
 }
+
 
